8f9fd964903e15c6325d9f8d101c91e49227e64c
Tcauley91 <caleytyler@gmail.com>
2021-02-14 21:55:40 -0500

    updating title

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,8 +22,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Tyler Cauley`,
+        short_name: `Tyler Cauley`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
