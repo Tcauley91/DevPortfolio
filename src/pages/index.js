c6edf5fe928ea694682d48d338cfa37810b22fc8
Tcauley91 <caleytyler@gmail.com>
2021-02-14 21:56:45 -0500

    eiditng index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default () => {
         <meta charSet="utf-8" />
         <title>{title || 'Tyler Cauley'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Tyler Cauley Portfolio'} />
       </Helmet>
       <App />
     </>
